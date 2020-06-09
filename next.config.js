8a7285ed431a01644eb853ef1bd7d2430185b0af
Greg Nicholson <greg@theme44.com>
2020-06-09 18:44:49 +0100

    Fix netlify loading env

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@ const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   webpack(config) {
-    config.plugins.push(new Dotenv());
+    config.plugins.push(new Dotenv({
+      systemvars: true,
+    }));
     return config;
   },
 };
